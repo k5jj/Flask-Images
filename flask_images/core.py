b7c3362c09c930ec8f50cdfbe7535e6b67f66fec
Erik Andrejko <eandrejko@gmail.com>
2017-02-28 20:19:50 -0800

    Python 3 compatibility with hashlib.
    Ensure hashing of encoded URL. Avoids:
    
    Traceback (most recent call last):
      File "/opt/conda/lib/python3.5/site-packages/flask/app.py", line 1612, in full_dispatch_request
        rv = self.dispatch_request()
      File "/opt/conda/lib/python3.5/site-packages/flask/app.py", line 1598, in dispatch_request
        return self.view_functions[rule.endpoint](**req.view_args)
      File "/opt/conda/lib/python3.5/site-packages/Flask_Images-2.1.2-py3.5.egg/flask_images/core.py", line 311, in handle_request
        hashlib.md5(remote_url).hexdigest() + os.path.splitext(parsed.path)[1]
    TypeError: Unicode-objects must be encoded before hashing

--- flask_images/core.py
+++ flask_images/core.py
@@ -306,10 +306,9 @@ class Images(object):
 
             # Download the remote file.
             makedirs(current_app.config['IMAGES_CACHE'])
-            remote_url = remote_url.encode("utf-8")
             path = os.path.join(
                 current_app.config['IMAGES_CACHE'],
-                hashlib.md5(remote_url).hexdigest() + os.path.splitext(parsed.path)[1]
+                hashlib.md5(remote_url.encode("utf-8")).hexdigest() + os.path.splitext(parsed.path)[1]
             )
 
             if not os.path.exists(path):
